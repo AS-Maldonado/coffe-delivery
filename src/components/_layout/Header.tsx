fd8343a69520ea9cdd01d6e564a2b3f5590a00c7
Augusto Maldonado <augusto.maldonado@openk.com.br>
2025-01-31 18:07:32 -0300

    feat: adicionando quantidade de carrinho no icone do topo

--- src/components/_layout/Header.tsx
+++ src/components/_layout/Header.tsx
@@ -1,8 +1,12 @@
 import { MapPin, ShoppingCart } from "lucide-react";
 import Logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
 
 export function Header() {
+  const { cart } = useContext(CartContext);
+
   return (
     <header className="py-8">
       <nav className="flex justify-between">
@@ -19,9 +23,12 @@ export function Header() {
           </div>
           <Link
             to="/checkout"
-            className="cursor-pointer rounded-md bg-yellow_light p-2 text-yellow_dark hover:bg-yellow_dark hover:text-yellow_light"
+            className="relative cursor-pointer rounded-md bg-yellow_light p-2 text-yellow_dark hover:bg-yellow_dark hover:text-yellow_light"
           >
             <ShoppingCart />
+            <span className="absolute -right-3 -top-3 h-6 w-6 rounded-full bg-yellow_dark text-center text-white">
+              {cart.length}
+            </span>
           </Link>
         </div>
       </nav>
